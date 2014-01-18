13e7e5f730c9f43bb2440db696a956075b287d48
Henning Dodenhof <henning.dodenhof@gmail.com>
2014-01-18 13:54:51 +0100

    Add sample app

---
+++ sample/src/main/java/de/hdodenhof/circleimageview/sample/MainActivity.java
@@ -0,0 +1,37 @@
+package de.hdodenhof.circleimageview.sample;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainFragment())
+                    .commit();
+        }
+    }
+
+    public static class MainFragment extends Fragment {
+
+        public MainFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
