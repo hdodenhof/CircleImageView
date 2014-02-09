6e8c49c9a49681a9b7a957474a86824bd231ae30
Henning Dodenhof <henning.dodenhof@gmail.com>
2014-02-09 16:19:12 +0100

    Get rid of magic number in favor of a constant

--- circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
+++ circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -61,7 +61,7 @@ public class CircleImageView extends ImageView {
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleImageView, defStyle, 0);
 
-        mBorderWidth = a.getDimensionPixelSize(R.styleable.CircleImageView_border_width, 0);
+        mBorderWidth = a.getDimensionPixelSize(R.styleable.CircleImageView_border_width, DEFAULT_BORDER_WIDTH);
         mBorderColor = a.getColor(R.styleable.CircleImageView_border_color, DEFAULT_BORDER_COLOR);
 
         a.recycle();
