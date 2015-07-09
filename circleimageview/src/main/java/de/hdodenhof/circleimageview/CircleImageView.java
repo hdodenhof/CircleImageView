f5d27e1cf22d9471f49d2e4dca92dfdf926debad
1liufeilong <1liufeilong@163.com>
2015-07-09 16:43:18 +0800

    Update CircleImageView.java

--- circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
+++ circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -217,6 +217,10 @@ public class CircleImageView extends ImageView {
         if (drawable instanceof BitmapDrawable) {
             return ((BitmapDrawable) drawable).getBitmap();
         }
+        //Finals FIXED HTC ONE BUGS
+        if(drawable.getIntrinsicWidth()==0||drawable.getIntrinsicHeight()==0){
+        	return null;
+        }
 
         try {
             Bitmap bitmap;
