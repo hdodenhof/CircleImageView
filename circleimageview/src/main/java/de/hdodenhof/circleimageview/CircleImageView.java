1520cbc013ff73c2a4b28379a6fb9acfccca3d68
Keita Watanabe <Nal3eCh@gmail.com>
2018-09-27 11:24:38 +0900

    Migration to androidx
    Because using support library now, migration to androidx.

--- circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
+++ circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -33,15 +33,15 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.ColorInt;
-import android.support.annotation.ColorRes;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewOutlineProvider;
 import android.widget.ImageView;
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.RequiresApi;
 
 public class CircleImageView extends ImageView {
 
