f03cd288f35a51a3ea854282fb483d8238559382
Henning Dodenhof <henning.dodenhof@gmail.com>
2020-01-01 12:53:03 +0100

    Update copyright date range

--- circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
+++ circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 - 2019 Henning Dodenhof
+ * Copyright 2014 - 2020 Henning Dodenhof
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
