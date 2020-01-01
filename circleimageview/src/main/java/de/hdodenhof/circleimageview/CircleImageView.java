hdodenhof/CircleImageView

commit 528125cae927614a63ffc0b9314cd8b9fd6326fd

Remove unnecessary null check

circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -290,9 +290,7 @@ public class CircleImageView extends ImageView {
     }
 
     private void applyColorFilter() {
-        if (mBitmapPaint != null) {
-            mBitmapPaint.setColorFilter(mColorFilter);
-        }
+        mBitmapPaint.setColorFilter(mColorFilter);
     }
 
     private Bitmap getBitmapFromDrawable(Drawable drawable) {

commit 4b8cc02e2294f3711dbe273d2973d9f84d73718e

Guard setting color filter by null check
This fixes a regression introduced with 528125cae927614a63ffc0b9314cd8b9fd6326fd.
Add comment to make sure the check stays this time. See #326 and #105.

circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -291,8 +291,13 @@ public class CircleImageView extends ImageView {
         return mColorFilter;
     }
 
+    @SuppressWarnings("ConstantConditions")
     private void applyColorFilter() {
-        mBitmapPaint.setColorFilter(mColorFilter);
+        // This might be called from setColorFilter during ImageView construction
+        // before member initialization has finished on API level <= 19.
+        if (mBitmapPaint != null) {
+            mBitmapPaint.setColorFilter(mColorFilter);
+        }
     }
 
     private Bitmap getBitmapFromDrawable(Drawable drawable) {