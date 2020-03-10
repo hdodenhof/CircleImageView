db568badf0f568a6b37947cfd547dbbda25a6268
Henning Dodenhof <henning.dodenhof@gmail.com>
2020-03-10 12:57:45 +0100

    Add TransitionDrawable to sample app

--- sample/src/main/java/de/hdodenhof/circleimageview/sample/MainActivity.java
+++ sample/src/main/java/de/hdodenhof/circleimageview/sample/MainActivity.java
@@ -1,7 +1,10 @@
 package de.hdodenhof.circleimageview.sample;
 
 import android.app.Activity;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView iv = findViewById(R.id.iv2);
+
+        (new Handler()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ((TransitionDrawable) iv.getDrawable()).startTransition(500);
+            }
+        }, 2000);
     }
 
 }
