ec54e53022dd1dd1e0dcd84fb944b95d437366cc
Henning Dodenhof <henning.dodenhof@gmail.com>
2020-03-13 13:02:21 +0100

    Remove pointless override of getScaleType

--- circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
+++ circleimageview/src/main/java/de/hdodenhof/circleimageview/CircleImageView.java
@@ -124,11 +124,6 @@ public class CircleImageView extends ImageView {
     }
 
     @Override
-    public ScaleType getScaleType() {
-        return SCALE_TYPE;
-    }
-
-    @Override
     public void setScaleType(ScaleType scaleType) {
         if (scaleType != SCALE_TYPE) {
             throw new IllegalArgumentException(String.format("ScaleType %s not supported.", scaleType));
